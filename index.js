6e18a9f38f95092e1e5017d89e031ed1a0f31640
nirdeshtiwari432 <nirdesh26cs081@satiengg.in>
2025-10-24 15:41:56 +0530

    cros3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "https://turningpointvidisha.vercel.app/", // Replace with your frontend URL
+    origin: "https://turningpointvidisha.vercel.app", // Replace with your frontend URL
     credentials: true,                                  // allow cookies/sessions
   })
 );
